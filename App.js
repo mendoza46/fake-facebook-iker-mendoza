cf6f306b663f9872cfbe6b00a28ccdf6dd9ee72d
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-10-04 13:40:49 +0200

    [NEW] configurar NavigatorContainer

--- App.js
+++ App.js
@@ -3,18 +3,59 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
 import Header from './components/Header';
+import Home from './screens/Home';
+import Friends from './screens/Friends';
+import Groups from './screens/Groups';
+import Menu from './screens/Menu';
+import Notifications from './screens/Notifications';
+import Profile from './screens/Profile';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return(
     <>
     <StatusBar
-    backgroundColor='#000000'
+    backgroundColor='red'
     barStyle='dark-content'
     />
     <ScrollView
       backgroundColor='green'
     >
       <Header/>
+      <NavigationContainer>
+        <Tab.Navigator
+        swipeEnabled='true'
+        tabBarOptions={{
+          showIcon: true,
+          showLabel: false,
+          activeTintColor: '#3a86e9',
+          inactiveTintColor: '#9F9F9F',
+        }}
+        screenOptions={({route}) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if(route.name === 'Home') iconName = 'home';
+            else if(route.name === 'Friends') iconName = 'account-multiple-outline';
+            else if(route.name === 'Groups') iconName = 'account-group'
+            else if(route.name === 'Profile') iconName = 'account-group'
+            else if(route.name === 'Notifications') iconName = 'bell-outline'
+            else if(route.name === 'Menu') iconName = 'menu'
+            return <MaterialCommunityIcons name={iconName} size={26} color={color}/>
+          },
+        })}
+        >
+          <Tab.Screen name="Home" component={Home}/>
+          <Tab.Screen name="Friends" component={Friends}/>
+          <Tab.Screen name="Groups" component={Groups}/>
+          <Tab.Screen name="Profile" component={Profile}/>
+          <Tab.Screen name="Notifications" component={Notifications}/>
+          <Tab.Screen name="Menu" component={Menu}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </ScrollView>
     </>
   )
