c3437253f5c235c96fe9b366f8cbb1fb082e0965
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-10-05 11:02:19 +0200

    [UPDATE] perfiles de usuarios

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 
 import ToolBar from "../components/ToolBar";
 import MainSeparator from "../components/MainSeparator";
+import Users from "../components/Users";
 
 const View = styled.View`
     flex: 1;
@@ -13,6 +14,7 @@ const Home = () => {
         <View>
             <ToolBar />
             <MainSeparator />
+            <Users />
         </View>
     )
 }
