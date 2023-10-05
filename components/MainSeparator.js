d537303b3ded57f7d19d9ff597cabe53f4d5ece0
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-10-05 10:12:12 +0200

    [NEW] MainSeparator

---
+++ components/MainSeparator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from 'styled-components/native';
+
+const Separator = styled.View`
+    width: 100%;
+    height: 10px;
+    background: #BDBDBD;
+    margin: 12px 0 0 0;`
+
+const MainSeparator = () => {
+    return(
+        <Separator />
+    )
+}
+
+export default MainSeparator;
