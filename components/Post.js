775d1bd184c8891ed0cf34b1a98e443223ae47b2
mendoza46 <iker.mendoza@ikasle.aeg.eus>
2023-10-05 13:18:24 +0200

    [UPDATE] cambiar nombre al fichero fakeuser

---
+++ components/Post.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View  } from "react-native";
+import styled from 'styled-components/native';
+import Avatar from "./Avatar";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+
+
+
